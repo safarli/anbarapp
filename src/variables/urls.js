6ae3ba056ede99abce60fe2a3e801f448b3c2d3a
Ofis PC <ccsafarli@gmail.com>
2021-06-03 15:35:18 +0400

    url changed to aws

--- src/variables/urls.js
+++ src/variables/urls.js
@@ -1,5 +1,5 @@
 
-const url = "http://localhost:9999"
-// const url = "https://anbar.wavevo.com"
+// const url = "http://localhost:9999"
+const url = "https://anbar.wavevo.com"
 
 module.exports = url;
