a5ceb0871289030db1c3299e417ece546ee5205f
Ofis PC <ccsafarli@gmail.com>
2021-06-08 19:08:33 +0400

    a commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,10 +13,12 @@
           <v-tabs-slider color="yellow darken-1"></v-tabs-slider>
           <v-tab>
             <v-icon>mdi-login-variant</v-icon>
-            GİRİŞ</v-tab>
+            GİRİŞ</v-tab
+          >
           <v-tab>
             <v-icon>mdi-account-plus</v-icon>
-            QEYDİYYAT</v-tab>
+            QEYDİYYAT</v-tab
+          >
           <v-tab-item>
             <v-card-text>
               <v-form ref="login_form">
@@ -26,37 +28,49 @@
                   type="email"
                 ></v-text-field>
                 <v-text-field
-                  :rules="passwRules"
+                  counter="60"
                   label="Şifrə"
                   type="password"
+                  :rules="passwRules"
                 ></v-text-field>
-                <v-text-field label="CAPTCHA"></v-text-field>
-                <v-btn @click="logIn" dark color="teal darken-2">
-                  GİRİŞ
-                  <v-icon>mdi-login</v-icon>
-                </v-btn>
+                <v-btn @click="logIn" dark color="teal darken-2" class="mt-2"> GİRİŞ </v-btn>
               </v-form>
             </v-card-text>
           </v-tab-item>
           <v-tab-item>
             <v-card-text>
               <v-form ref="register_form">
-                <v-text-field label="Ad"></v-text-field>
-                <v-text-field label="Soyad"></v-text-field>
+                <v-text-field
+                  label="Ad"
+                  :rules="adRules"
+                  counter="255"
+                ></v-text-field>
+                <v-text-field
+                  label="Soyad"
+                  :rules="soyadRules"
+                  counter="255"
+                ></v-text-field>
                 <v-text-field
                   :rules="emailRules"
                   label="E-mail"
                   type="email"
                 ></v-text-field>
-                <v-text-field
-                  :rules="passwRules"
-                  label="Şifrə"
-                  type="password"
-                ></v-text-field>
-                <v-btn @click="register" dark color="pink">
-                  QEYDİYYAT
-                  <v-icon>mdi-login</v-icon>
-                </v-btn>
+                <v-tooltip bottom content-class="grey darken-4" open-delay="300" :open-on-hover="false">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-bind="attrs"
+                      v-on="on"
+                      :rules="passwRules"
+                      label="Şifrə"
+                      type="password"
+                    ></v-text-field>
+                  </template>
+                  <span
+                    >Şifrə 8 simvoldan az olmayaraq, ən az 1 rəqəm, 1 kiçik
+                    hərf, 1 böyük hərfdən ibarət olmalıdır</span
+                  >
+                </v-tooltip>
+                <v-btn @click="register" dark color="pink" class="mt-2"> QEYDİYYAT </v-btn>
               </v-form>
             </v-card-text>
           </v-tab-item>
@@ -79,8 +93,22 @@ export default {
           (v && v.length < 120) ||
           "E-Poçt ünvanı 120 xarakterdən qısa olmalıdır",
       ],
-      passwRules: [(v) => !!v || "Şifrə daxil edin"],
+      passwRules: [
+        (v) => !!v || "Şifrə daxil edin",
+        (v) =>
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(v) ||
+          "Qaydalara uyğun şifrə daxil edin",
+      ],
+      adRules: [(v) => !!v || "Adınızı daxil edin"],
+      soyadRules: [(v) => !!v || "Soyadınızı daxil edin"],
     };
+  },
+  computed: {
+    randomQuestion() {
+      const num_1 = Math.round(Math.random() * 10);
+      const num_2 = Math.round(Math.random() * 10);
+      return `${num_1} + ${num_2} = ?`;
+    },
   },
   methods: {
     logIn() {
